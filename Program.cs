251e343e0899e0502048ee614d1038e1c6af5e7b
Charlydob <carloseduardooliveirabarbosa@gmail.com>
2025-07-09 19:58:41 +0200

    Fix: Added missing using for AddScoped

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using AppEroskiNueva;
+using Microsoft.Extensions.DependencyInjection;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
--- obj/Release/net9.0/AppEroskiNueva.AssemblyInfo.cs
+++ obj/Release/net9.0/AppEroskiNueva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppEroskiNueva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+825e3561a6c1b38117c6fcdf0c8783286b00a202")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ed3e660165a087d34db244bb6d0f9b114e85d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppEroskiNueva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppEroskiNueva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
